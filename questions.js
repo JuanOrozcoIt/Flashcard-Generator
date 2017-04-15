27406950051d49f50e4c31caebb7f4b91772609e
JuanOrozcoIt <Juan.Orozco.it@gmail.com>
2017-04-15 12:03:23 -0700

    questions and answers

---
+++ questions.js
@@ -0,0 +1,26 @@
+exports.questions = [
+	{
+		full: '1. if the ball rebounds from a goal post it is still in play.',
+		cloze: 'true'
+	},
+	{
+		full: '2. the goal kick must be taken by the goalkeeper.',
+		cloze: 'false'
+	},
+	{
+		full: '3. the kick off is always played forward into the half of the opponent.',
+		cloze: 'false'
+	},
+	{
+		full: '4. after a goal the goal keeper takes a goal kick to start again.',
+		cloze: 'false'
+	},
+	{
+		full: '5. a goal can be scored directly from a corner kick.',
+		cloze: 'true'
+	},
+	{
+		full: '6. a corner kick can be taken from either side of the goal.',
+		cloze: 'true'
+	}
+]
